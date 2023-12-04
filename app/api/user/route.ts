cf8ac535088e1a567c3053abedbde5fc55786c50
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2023-12-04 16:04:41 +0500

    bug fixes v1

--- app/api/user/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({ message: "Hello, World!" });
-}
-
-export async function POST(req: NextRequest) {
-  console.log("hello from POST");
-}
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Card({ children, className }: { children: React.ReactNode, className: string }) {
   return (
-    <div className={`bg-white p-4 shadow rounded ${className}`}>
+    <div className={`bg-white p-4 shadow rounded-lg ${className}`}>
       {children}
     </div>
   )
