900b7fdbd5b3d67999934f3cec1a3285a1e6b734
Abdullah Zahid <zahidabdullah753@gmail.com>
2023-12-11 00:15:31 +0500

    Add modal Completed

--- app/content/files/page.tsx
+++ app/content/files/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import Link from "next/link";
 import Heading from "@/components/Heading";
@@ -11,6 +13,7 @@ import OuterText from "@/components/OuterText";
 import { fi } from "date-fns/locale";
 import TransparentButton from "@/components/TransparentButton";
 import OuterLink from "@/components/Link";
+import { AddModal } from "@/components/modals/ContainerBarModals/AddModal";
 
 const Files = () => {
   return (
@@ -18,7 +21,7 @@ const Files = () => {
       <div className="flex justify-between items-center mb-3">
         <Heading className="pb-0">Files</Heading>
         <div>
-          <LinkMini href="">Upload File</LinkMini>
+          <FilledButton>Upload File</FilledButton>
         </div>
       </div>
       <Card >
@@ -26,14 +29,7 @@ const Files = () => {
           <div className="flex justify-between items-center px-5">
             <div className="flex items-center gap-2">
               <button className="w-10 h-7 py-1 px-3 bg-gray-200 rounded-md text-xs font-semibold text-gray-900 hover:bg-gray-100 transition">All</button>
-              <button className="py-1 px-2 hover:bg-gray-100 rounded-md">
-                <Image
-                  src="/PlusButton.svg"
-                  width={20}
-                  height={20}
-                  alt="Plus button Image"
-                />
-              </button>
+              <AddModal/>
             </div>
             <div className="flex items-center gap-3">
               <button className="py-1 px-1 border rounded-md flex items-center shadow-sm shadow-neutral-200">
