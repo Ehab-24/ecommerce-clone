8be95d09e033eaa97ab06e84ed721a11d7b01fa3
Abdullah Zahid <zahidabdullah753@gmail.com>
2023-12-12 23:58:28 +0500

    search button complete

--- components/CardTopBar.tsx
+++ components/CardTopBar.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { AddModal } from './modals/ContainerBarModals/AddModal'
 import { SortPopover } from './popovers/ContainerbarPopover'
+import { Input } from "@/components/ui/input"
 import Image from 'next/image'
 
 const CardTopBar = () => {
+
+    const [searchclicked, setSearchClicked] = React.useState(false)
+
+    const clicked = () => {
+        if (searchclicked === false) {
+            setSearchClicked(true)
+        }
+        else {
+            setSearchClicked(false)
+        }
+
+    }
+
     return (
         <div className="py-2 w-full border-b border-gray-300 ">
-            <div className="flex justify-between items-center px-5">
+            {searchclicked ? (<div className="flex justify-between items-center px-5">
                 <div className="flex items-center gap-2">
                     <button className="w-10 h-7 py-1 px-3 bg-gray-200 rounded-md text-xs font-semibold text-gray-900 hover:bg-gray-100 transition">All</button>
                     <AddModal />
                 </div>
                 <div className="flex items-center gap-3">
-                    <button className="py-1 px-1 border rounded-md flex items-center shadow-sm shadow-neutral-200">
+                    <button onClick={clicked} className="py-1 px-1 border rounded-md flex items-center shadow-sm shadow-neutral-200">
                         <Image
                             src="/SearchIcon.svg"
                             width={20}
@@ -29,7 +43,21 @@ const CardTopBar = () => {
 
                     <SortPopover />
                 </div>
-            </div>
+            </div>) :
+                (
+                    <div className="flex justify-between items-center px-5">
+                        <Input className='h-8 w-[85%]' type="Text" placeholder="Searching in All" />
+                        <div className='flex items-center gap-3'>
+                            <button className='text-xs text-gray-700 font-semibold' onClick={clicked}>
+                                Cancel
+                            </button>
+                            <button className="h-7 py-1 px-2 rounded-md text-xs font-semibold text-gray-400  bg-gray-100 transition">
+                                Save as
+                            </button>
+                            <SortPopover />
+                        </div>
+                    </div>
+                )}
         </div>
     )
 }
