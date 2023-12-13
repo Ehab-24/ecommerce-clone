0804484db5fb39bd85c923b1b6926cfd500bf239
Abdullah Zahid <zahidabdullah753@gmail.com>
2023-12-13 15:59:08 +0500

    tooltip added

--- components/Content/CopyHoverCard.tsx
+++ components/Content/CopyHoverCard.tsx
@@ -3,25 +3,31 @@ import {
     HoverCardContent,
     HoverCardTrigger,
 } from "@/components/ui/hover-card"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 import { CalendarIcon } from 'lucide-react'
 import Image from "next/image"
 import { Button } from 'react-day-picker'
 
 const CopyHoverCard = () => {
     return (
-        <HoverCard>
-            <HoverCardTrigger asChild>
-                <div className="cursor-pointer rounded-lg border w-7 h-7 flex justify-center items-center shadow-sm shadow-neutral-200">
+        <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+          <div className="cursor-pointer rounded-lg border w-7 h-7 flex justify-center items-center shadow-sm shadow-neutral-200">
                     <Image src="/Linksvg.svg" width={22} height={22} alt="Link Image" />
                 </div>
-            </HoverCardTrigger>
-            <HoverCardContent className="w-20 p-2 cursor-pointer">
-                <p className="font-medium flex justify-center items-center text-xs text-gray-800">
-                    Copy Link
-                </p>
-
-            </HoverCardContent>
-        </HoverCard>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="font-medium flex justify-center items-center text-xs text-gray-800" >Copy link</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     )
 }
 
