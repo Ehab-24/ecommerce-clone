5890ee2baa80e4f73fda3aebc0cf1547f57cf078
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2023-12-16 22:40:09 +0500

    edited customers table and home

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -1,30 +1,57 @@
-import React from "react"
+import React from "react";
 
-export default function Checkbox({ id, label, checked, disabled = false, onChange }: { id: string, label?: string, checked?: boolean, disabled?: boolean, onChange: React.ChangeEventHandler<HTMLInputElement> }) {
+export default function Checkbox({
+  id,
+  label,
+  checked,
+  disabled = false,
+  onChange,
+}: {
+  id: string;
+  label?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}) {
   return (
     <>
       <div className="inline-flex items-center">
-        <label className="relative flex items-center rounded-full cursor-pointer" htmlFor="check">
-          <input type="checkbox"
+        <label
+          className="relative flex items-center rounded-full cursor-pointer"
+          htmlFor="check"
+        >
+          <input
+            type="checkbox"
             disabled={disabled}
             checked={checked}
             onChange={onChange}
-            className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border-2 border-blue-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
-            id={id} />
-          <span
-            className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
-              stroke="currentColor" strokeWidth="1">
-              <path fillRule="evenodd"
+            className="before:content[''] peer relative h-4 w-4 cursor-pointer appearance-none rounded border border-neutral-500 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+            id={id}
+          />
+          <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3.5 w-3.5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              stroke="currentColor"
+              strokeWidth="1"
+            >
+              <path
+                fillRule="evenodd"
                 d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"></path>
+                clipRule="evenodd"
+              ></path>
             </svg>
           </span>
         </label>
-        <label className="mt-px pl-3 text-gray-900 text-xs cursor-pointer select-none" htmlFor={id}>
+        <label
+          className="mt-px pl-3 text-gray-900 text-xs cursor-pointer select-none"
+          htmlFor={id}
+        >
           {label}
         </label>
       </div>
     </>
-  )
+  );
 }
--- components/popovers/Customer.tsx
+++ components/popovers/Customer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import InputSearch from "@/components/InputSearch";
 import Input from "../Input";
 
 interface Customer {
